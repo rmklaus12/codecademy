56978ec5163b3d3c47c63387c2ceca94f2e5dfab
Robin Klaus Dunbar <rmklaus12@gmail.com>
2019-05-29 19:41:42 +1000

    Create message.js

---
+++ message.js
@@ -0,0 +1,16 @@
+import MessageMixer from './messageMixer';
+
+import { countCharacter, capitalizeFirstCharacterOfWords, reverseWord, reverseAllWords, replaceFirstOccurence, replaceAllOccurrences, encode, palindrome, pigLatin} from './messageMixer';
+
+function displayMessage() {
+  console.log(countCharacter("What is the color of the sky?", "t"));
+  console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+  console.log(reverseWord("What is the color of the sky?"));
+  console.log(reverseAllWords("What is the color of the sky?"));
+  console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+  console.log(encode("What is the color of the sky?"));
+  console.log(palindrome("Where is my taco?"));
+  console.log(pigLatin("Pabst Blue Ribbon", "ay "));
+}
+
+displayMessage();
